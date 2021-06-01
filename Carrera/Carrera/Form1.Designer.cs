ea5c7d9ee84d97a625e53a15d9d37b35b6ba95d0
Aracely234 <84871755+Aracely234@users.noreply.github.com>
2021-06-01 16:50:36 -0700

    Hola

---
+++ Carrera/Carrera/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Carrera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.caballo1 = new System.Windows.Forms.PictureBox();
+            this.caballo2 = new System.Windows.Forms.PictureBox();
+            this.caballo3 = new System.Windows.Forms.PictureBox();
+            this.caballo4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.caballo5 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sus = new System.Windows.Forms.Button();
+            this.res = new System.Windows.Forms.Button();
+            this.ab = new System.Windows.Forms.Button();
+            this.jo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // caballo1
+            // 
+            this.caballo1.BackColor = System.Drawing.Color.Transparent;
+            this.caballo1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("caballo1.BackgroundImage")));
+            this.caballo1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.caballo1.Location = new System.Drawing.Point(0, 0);
+            this.caballo1.Name = "caballo1";
+            this.caballo1.Size = new System.Drawing.Size(121, 73);
+            this.caballo1.TabIndex = 0;
+            this.caballo1.TabStop = false;
+            // 
+            // caballo2
+            // 
+            this.caballo2.BackColor = System.Drawing.Color.Transparent;
+            this.caballo2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("caballo2.BackgroundImage")));
+            this.caballo2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.caballo2.Location = new System.Drawing.Point(0, 79);
+            this.caballo2.Name = "caballo2";
+            this.caballo2.Size = new System.Drawing.Size(121, 73);
+            this.caballo2.TabIndex = 1;
+            this.caballo2.TabStop = false;
+            // 
+            // caballo3
+            // 
+            this.caballo3.BackColor = System.Drawing.Color.Transparent;
+            this.caballo3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("caballo3.BackgroundImage")));
+            this.caballo3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.caballo3.Location = new System.Drawing.Point(0, 158);
+            this.caballo3.Name = "caballo3";
+            this.caballo3.Size = new System.Drawing.Size(121, 73);
+            this.caballo3.TabIndex = 2;
+            this.caballo3.TabStop = false;
+            // 
+            // caballo4
+            // 
+            this.caballo4.BackColor = System.Drawing.Color.Transparent;
+            this.caballo4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("caballo4.BackgroundImage")));
+            this.caballo4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.caballo4.Location = new System.Drawing.Point(0, 237);
+            this.caballo4.Name = "caballo4";
+            this.caballo4.Size = new System.Drawing.Size(121, 73);
+            this.caballo4.TabIndex = 3;
+            this.caballo4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(352, 390);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(52, 48);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // caballo5
+            // 
+            this.caballo5.BackColor = System.Drawing.Color.Transparent;
+            this.caballo5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("caballo5.BackgroundImage")));
+            this.caballo5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.caballo5.Location = new System.Drawing.Point(0, 316);
+            this.caballo5.Name = "caballo5";
+            this.caballo5.Size = new System.Drawing.Size(121, 73);
+            this.caballo5.TabIndex = 5;
+            this.caballo5.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(720, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(19, 389);
+            this.panel1.TabIndex = 6;
+            // 
+            // sus
+            // 
+            this.sus.Location = new System.Drawing.Point(190, 404);
+            this.sus.Name = "sus";
+            this.sus.Size = new System.Drawing.Size(75, 23);
+            this.sus.TabIndex = 7;
+            this.sus.Text = "Pausar";
+            this.sus.UseVisualStyleBackColor = true;
+            this.sus.Click += new System.EventHandler(this.sus_Click);
+            // 
+            // res
+            // 
+            this.res.Location = new System.Drawing.Point(271, 404);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(75, 23);
+            this.res.TabIndex = 8;
+            this.res.Text = "Iniciar";
+            this.res.UseVisualStyleBackColor = true;
+            this.res.Click += new System.EventHandler(this.res_Click);
+            // 
+            // ab
+            // 
+            this.ab.Location = new System.Drawing.Point(410, 404);
+            this.ab.Name = "ab";
+            this.ab.Size = new System.Drawing.Size(75, 23);
+            this.ab.TabIndex = 9;
+            this.ab.Text = "Abort";
+            this.ab.UseVisualStyleBackColor = true;
+            this.ab.Click += new System.EventHandler(this.ab_Click);
+            // 
+            // jo
+            // 
+            this.jo.Location = new System.Drawing.Point(491, 404);
+            this.jo.Name = "jo";
+            this.jo.Size = new System.Drawing.Size(75, 23);
+            this.jo.TabIndex = 10;
+            this.jo.Text = "Join";
+            this.jo.UseVisualStyleBackColor = true;
+            this.jo.Click += new System.EventHandler(this.jo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Carrera.Properties.Resources.istockphoto_157379821_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(751, 439);
+            this.Controls.Add(this.jo);
+            this.Controls.Add(this.ab);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.sus);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.caballo5);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.caballo4);
+            this.Controls.Add(this.caballo3);
+            this.Controls.Add(this.caballo2);
+            this.Controls.Add(this.caballo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.caballo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox caballo1;
+        private System.Windows.Forms.PictureBox caballo2;
+        private System.Windows.Forms.PictureBox caballo3;
+        private System.Windows.Forms.PictureBox caballo4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox caballo5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button sus;
+        private System.Windows.Forms.Button res;
+        private System.Windows.Forms.Button ab;
+        private System.Windows.Forms.Button jo;
+    }
+}
+
---
+++ Carrera/Carrera/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Carrera
+{
+    public partial class Form1 : Form
+    {
+
+        int a = 0, b = 0, c = 0, d = 0,f=0;
+        //definir los diferentes procesos
+        Thread c1;
+        Thread c2;
+        Thread c3;
+        Thread c4;
+        Thread c5;
+
+        public void nuevo()
+        {
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            c1 = new Thread(new ThreadStart(funcion1));
+            c2 = new Thread(new ThreadStart(funcion2));
+            c3 = new Thread(new ThreadStart(funcion3));
+            c4 = new Thread(new ThreadStart(funcion4));
+            c5 = new Thread(new ThreadStart(funcion5));
+            //sus.Enabled = true;
+            //jo.Enabled = true;
+            //res.Enabled = true;
+            //ab.Enabled = true;
+
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+        
+        public void funcion1()
+        {
+            while (true)
+            {
+                a++; caballo1.Location = new System.Drawing.Point(a, 0);
+                Thread.Sleep(1);
+                if ((panel1.Left - caballo1.Right) <-10)
+                {
+                    this.c1.Abort();
+                    break;
+                }
+
+            }
+        }
+        public void funcion2()
+        {
+            while (true)
+            {
+                b++; caballo2.Location = new System.Drawing.Point(b, 79);
+                Thread.Sleep(30);
+                if ((panel1.Left - caballo2.Right) < -10)
+                {
+                    this.c2.Abort();
+                    break;
+                }
+
+            }
+        }
+        public void funcion3()
+        {
+            while (true)
+            {
+                c++; caballo3.Location = new System.Drawing.Point(c, 158);
+                Thread.Sleep(30);
+                if ((panel1.Left - caballo3.Right) < -10)
+                {
+                    this.c3.Abort();
+                    break;
+                }
+
+            }
+        }
+        public void funcion4()
+        {
+            while (true)
+            {
+                d++; caballo4.Location = new System.Drawing.Point(d, 237);
+                Thread.Sleep(30);
+                if ((panel1.Left - caballo4.Right) < -10)
+                {
+                    this.c4.Abort();
+                    break;
+                }
+
+            }
+        }
+        public void funcion5()
+        {
+            while (true)
+            {
+                f++; caballo5.Location = new System.Drawing.Point(f, 316);
+                Thread.Sleep(30);
+                if ((panel1.Left - caballo5.Right) < -10)
+                {
+                    this.c5.Abort();
+                    break;
+                }
+
+            }
+        }
+
+        private void sus_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                c1.Suspend();
+                c2.Suspend();
+                c3.Suspend();
+                c4.Suspend();
+                c5.Suspend();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void res_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                c1.Resume();
+                c2.Resume();
+                c3.Resume();
+                c4.Resume();
+                c5.Resume();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void ab_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                while (true)
+                {
+                    c1.Abort();
+                    c2.Abort();
+                    c3.Abort();
+                    c4.Abort();
+                    c5.Abort();
+                    a = 0;
+                    b = 0;
+                    c = 0;
+                    d = 0;
+                    f = 0;
+                    caballo1.Location = new System.Drawing.Point(0, 0);
+                    caballo2.Location = new System.Drawing.Point(0, 79);
+                    caballo3.Location = new System.Drawing.Point(0, 158);
+                    caballo4.Location = new System.Drawing.Point(0, 237);
+                    caballo5.Location = new System.Drawing.Point(0, 316);
+                    
+                }
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+
+        private void jo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                c1.Join();
+                c2.Join();
+                c3.Join();
+                c4.Join();
+                c5.Join();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Enabled = true; 
+            try
+            {
+                c1.Start();
+                c2.Start();
+                c3.Start();
+                c4.Start();
+                c5.Start();
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            pictureBox5.Enabled = false;
+        }
+
+        
+
+    }
+}
